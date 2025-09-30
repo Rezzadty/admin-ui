d2c83a4604645eb5d6046f67ca03bab716fd84c5
Rezzadty <rezaadityaprabowo474@gmail.com>
2025-09-30 11:05:08 +0700

    Using Tailwind Class Utility

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-blue-200">Vite + React</h1>
-      <h3>Reza Aditya Prabowo</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="min-h-screen bg-neutral-800 flex flex-col items-center justify-center text-center"> 
+        <div className="flex items-center justify-center space-x-8 mb-12">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} 
+            className="logo w-25 h-30" 
+            alt="Vite logo" 
+            />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img 
+            src={reactLogo} 
+            className="logo react w-25 h-30 animate-spin" 
+            alt="React logo" 
+            style={{ animationDuration: '10s' }} 
+            />
+          </a>
+        </div>
+        <h1 className="text-white text-8xl font-bold mb-16">Vite + React</h1>
+        <h3 className="text-white mb-2">Reza Aditya Prabowo</h3>
+        <div className="text-sm mt-3 space-y-6 mb-6">
+          <button 
+          onClick={() => setCount((count) => count + 1)}
+          className = "bg-neutral-900 text-white py-2 px-6 rounded-lg text-lg"
+          >
+            count is {count}
+          </button>
+          <p className="text-neutral-400">
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs text-neutral-400  ">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
